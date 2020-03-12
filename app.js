7f97766b6951ad87a1da751b43aadaaed23048f0
Haggreed <hugo.duvegheant@epita.fr>
2020-03-12 16:20:56 +0000

    enfin

---
+++ app.js
@@ -0,0 +1,14 @@
+function myAlphabetLength(table){
+    return table.length;
+}
+var Planets = ["Mecure","Venus","Earth","Mars","Jupiter"];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+var wowDatatypes =[1,'2',"3"];
+var hello = function(){
+    return "Hello World!";
+}
+console.log(hello());
